ea42694e711269c35c508e5951611c196951805f
Burak Kaya <boroppi01@gmail.com>
2018-10-11 01:09:43 -0400

    added a custom htmlhelper, modified the default bootstrap styles more

---
+++ mvc-proboosting/Helpers/CustomHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace mvc_proboosting.Helpers
+{
+    public static class CustomHelper
+    {
+        //Source : https://odetocode.com/blogs/scott/archive/2012/08/25/asp-net-mvc-highlight-current-link.aspx
+        public static MvcHtmlString MenuLink(this HtmlHelper helper, string text, string action, string controller)
+        {
+            var routeData = helper.ViewContext.RouteData.Values;
+            //get url controller for instance if it's boosters our url will show /boosters that way we can use a conditional statement and set the class accordingly
+            var currentController = routeData["controller"];
+            var currentAction = routeData["action"];
+
+            if (String.Equals(controller, currentController as string,
+                    StringComparison.OrdinalIgnoreCase))
+            {
+                return helper.ActionLink(
+                    text, action, controller, null,
+                    new { @class = "active" }
+                );
+            }
+            return helper.ActionLink(text, action, controller);
+        }
+
+
+
+    }
+}
